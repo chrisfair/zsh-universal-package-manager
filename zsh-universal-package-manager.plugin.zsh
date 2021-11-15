6becbefd58acc90d12f8f79fc7443284b99e25e6
Christopher Patrick Fair <christopherpfair@comcast.net>
2021-11-14 20:51:32 -0700

    corrected the if blocks

--- zsh-universal-package-manager.plugin.zsh
+++ zsh-universal-package-manager.plugin.zsh
@@ -39,7 +39,8 @@ is_homebrew_like() {
 
 
 # Pacman aliases
-if is_arch_like {
+if is_arch_like 
+then
   alias update='sudo pacman -Sy'
   alias upgrade='sudo pacman -Syu'
   alias install-package='sudo pacman -S'
@@ -47,10 +48,11 @@ if is_arch_like {
   alias search='pacman -Ss'
   alias list-packages='pacman -Q'
   alias update-mirrors='sudo pacman -Syy'
-}
+fi
 
 # Yay aliases
-if is_arch_like {
+if is_arch_like 
+then
   alias update='sudo yay -Sy'
   alias upgrade='sudo yay -Syu'
   alias install-package='sudo yay -S'
@@ -58,58 +60,63 @@ if is_arch_like {
   alias search='yay -Ss'
   alias list-packages='yay -Q'
   alias update-mirrors='sudo yay -Syy'
-}
+fi
 
 
 # apt-get aliases
-if is_debian_like {
+if is_debian_like 
+then
   alias update = 'sudo apt-get update'
   alias upgrade='sudo apt-get update && sudo apt-get upgrade'
   alias install-package='sudo apt-get install'
   alias remove='sudo apt-get remove'
   alias search='apt-cache search'
   alias list-packages='apt-cache pkgnames'
-} 
+fi
 
 # yum aliases
-if is_redhat_like{
+if is_redhat_like
+then
   alias update='sudo yum update'
   alias upgrade='sudo yum update && sudo yum upgrade'
   alias install-package='sudo yum install'
   alias remove='sudo yum remove'
   alias search='yum search'
   alias list-packages='yum list'
-}
+fi
 
 
 # emerge aliases
-if is_gentoo_like {
+if is_gentoo_like
+then
   alias update='sudo emerge --sync'
   alias upgrade='sudo emerge --sync && sudo emerge --update'
   alias install-package='sudo emerge'
   alias remove='sudo emerge -C'
   alias search='emerge -S'
   alias list-packages='emerge -p'
-} 
+fi 
 
 
 # apk aliases
-if is_alpine_like {
+if is_alpine_like
+then
   alias update='sudo apk update'
   alias upgrade='sudo apk update && sudo apk upgrade'
   alias install-package='sudo apk add'
   alias remove='sudo apk del'
   alias search='apk search'
   alias list-packages='apk info'
-}
+fi
 
 # brew aliases
-if is_homebrew_like {
+if is_homebrew_like 
+then
   alias update='brew update'
   alias upgrade='brew update && brew upgrade'
   alias install-package='brew install'
   alias remove='brew uninstall'
   alias search='brew search'
   alias list-packages='brew list'
-}
+fi
 
