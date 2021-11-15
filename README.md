4f63757313471d2e95b0d664bf38313f95338c1e
Christopher Patrick Fair <christopherpfair@comcast.net>
2021-11-14 20:36:08 -0700

    Initial commit for project

---
+++ README.md
@@ -0,0 +1,48 @@
+# universal-package-manager 
+
+This ZSH plugin abstracts the interface for all plugin managers that I
+can think of, including macos (when using brew), debian, redhat, gentoo, alpine,
+and arch based systems.  This will allow you to simply type the following command 
+instead of the distri system
+*upgrade - upgrades the system
+*install - installs a package
+*remove - removes a package
+*search - searchs for a package
+*list-packages - lists installed packages 
+*update-mirrors - updates mirrors for speed
+
+
+## Plugin Installation
+
+### Antigen
+
+If you use [Antigen](http://antigen.sharats.me/ "Antigen plugin manager for ZSH"),
+add the following line to your plugins list:
+
+`antigen bundle chrisfair/zsh-universal-package-manager`
+
+### Oh My Zsh
+
+If you use [Oh-My-Zsh](https://github.com/robbyrussell/oh-my-zsh "OMZ manager for ZSH"),
+add `zsh-universal-package-manager` to your `plugins` list and clone this repository to your
+`$ZSH_CUSTOM/plugins` directory:
+
+`$ ( cd $ZSH_CUSTOM/plugins && git clone https://github.com/chrisfair/zsh-universal-package-manager)`
+
+### Zgen
+
+If you're using [Zgen](https://github.com/tarjoilija/zgen), add
+
+`zgen load chrisfair/zsh-universal-package-manager`
+
+to your `.zshrc` with your other zgen plugin load commands.
+
+### Zplugin
+
+If you're using [Zplugin](https://github.com/zdharma/zplugin), add
+
+`zplugin light "chrisfair/zsh-universal-package-manager"`
+
+to your `.zshrc` with your other zplugin load commands.
+
+
---
+++ zsh-universal-package-manager.plugin.zsh
@@ -0,0 +1,121 @@
+# Copyright (c) 2021, Christopher Fair
+#
+# Universal Plugin Aliases for Zsh
+# There is nothing particularly zsh specific in this file.
+# feel free to use/modify/redistribute this file however you see fit.
+# Should work with bash, zsh, and fish.
+
+is_arch_like() {
+  [ -f /etc/arch-release ]
+}
+
+is_debian_like() {
+  [ -f /etc/debian_version ]
+}
+
+is_redhat_like() {
+  [ -f /etc/redhat-release ]
+} 
+
+is_gentoo_like() {
+  [ -f /etc/gentoo-release ]
+}
+
+is_alpine_like() {
+  [ -f /etc/alpine-release ]
+}
+
+is_void_like() {
+  [ -f /etc/void-release ]
+}
+
+is_macos_like() {
+  [ -f /etc/macos ]
+}
+
+is_homebrew_like() {
+  [ -f /usr/local/bin/brew ]
+} 
+
+
+# Pacman aliases
+if is_arch_like {
+  alias update='sudo pacman -Sy'
+  alias upgrade='sudo pacman -Syu'
+  alias install-package='sudo pacman -S'
+  alias remove='sudo pacman -R'
+  alias search='pacman -Ss'
+  alias list-packages='pacman -Q'
+  alias list-install-packageed-packages'pacman -Q'
+  alias update-mirrors='sudo pacman -Syy'
+}
+
+# Yay aliases
+if is_arch_like {
+  alias update='sudo yay -Sy'
+  alias upgrade='sudo yay -Syu'
+  alias install-package='sudo yay -S'
+  alias remove='sudo yay -R'
+  alias search='yay -Ss'
+  alias list-packages='yay -Q'
+  alias list-install-packageed-packages'yay -Q'
+}
+
+
+# apt-get aliases
+if is_debian_like {
+  alias update = 'sudo apt-get update'
+  alias upgrade='sudo apt-get update && sudo apt-get upgrade'
+  alias install-package='sudo apt-get install'
+  alias remove='sudo apt-get remove'
+  alias search='apt-cache search'
+  alias list-packages='apt-cache pkgnames'
+  alias list-install-packageed-packages'dpkg -l'
+} 
+
+# yum aliases
+if is_redhat_like{
+  alias update='sudo yum update'
+  alias upgrade='sudo yum update && sudo yum upgrade'
+  alias install-package='sudo yum install'
+  alias remove='sudo yum remove'
+  alias search='yum search'
+  alias list-packages='yum list'
+  alias list-install-packageed-packages'rpm -qa'
+}
+
+
+# emerge aliases
+if is_gentoo_like {
+  alias update='sudo emerge --sync'
+  alias upgrade='sudo emerge --sync && sudo emerge --update'
+  alias install-package='sudo emerge'
+  alias remove='sudo emerge -C'
+  alias search='emerge -S'
+  alias list-packages='emerge -p'
+  alias list-install-packageed-packages'emerge -p'
+} 
+
+
+# apk aliases
+if is_alpine_like {
+  alias update='sudo apk update'
+  alias upgrade='sudo apk update && sudo apk upgrade'
+  alias install-package='sudo apk add'
+  alias remove='sudo apk del'
+  alias search='apk search'
+  alias list-packages='apk info'
+  alias list-install-packageed-packages'apk info'
+}
+
+# brew aliases
+if is_homebrew_like {
+  alias update='brew update'
+  alias upgrade='brew update && brew upgrade'
+  alias install-package='brew install'
+  alias remove='brew uninstall'
+  alias search='brew search'
+  alias list-packages='brew list'
+  alias list-install-packageed-packages'brew list'
+}
+
